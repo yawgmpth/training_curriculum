4d7a534a39dedb34bfa575fd06560c93d8460041
yawgmpth <71745094+yawgmpth@users.noreply.github.com>
2020-10-11 15:57:59 +0900

    div 削除

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,15 +1,12 @@
 class CalendarsController < ApplicationController
 
-  # １週間のカレンダーと予定が表示されるページ
   def index
-
 
     get_week
     @plan = Plan.new
 
   end
   
-    # 予定の保存
   def create
     
     Plan.create(plan_params)
@@ -22,12 +19,11 @@ class CalendarsController < ApplicationController
     params.require(:plan).permit(:date, :plan)
   end
 
+
   def get_week
     week_days = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-
-    # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
+   
     @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
 
@@ -45,7 +41,6 @@ class CalendarsController < ApplicationController
         wday_num = wday_num - 7
       end
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
-
       @week_days.push(days)
     end
   
